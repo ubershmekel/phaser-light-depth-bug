1cb51f8cb403b3af497d62be77a0fd7fd2b4f92e
Yuval Greenfield <ubershmekel@gmail.com>
2022-04-01 23:31:49 -0700

    Example card data

---
+++ src/card-data.ts
@@ -0,0 +1,69 @@
+/*
+  * Mental health - mh
+  * Manager sentiment - mgr
+  * Friend sentiment - fr
+  * Money - money
+  * OKR Progress - prog
+*/
+
+interface CardData {
+  title: string;
+  mh?: number;
+  mgr?: number;
+  fr?: number;
+  money?: number;
+  prog?: number;
+  time?: number;
+}
+
+export const cards: CardData[] = [
+  {
+    title: "Take the boss to the gun range",
+    mgr: 5,
+    money: -6,
+  },
+  {
+    title: "Outsource work",
+    money: -6,
+    prog: 3,
+    mgr: -1,
+  },
+  {
+    title: "Throw a party",
+    // desc: "2x friend sentiment, -10 money",
+    money: -10,
+    fr: 5,
+    // TODO: 2x friend sent
+  },
+  {
+    title: "Say something 'smart' at the meeting",
+    mgr: 2,
+    fr: -3,
+  },
+  {
+    title: "Have a baby",
+    time: 9,
+    mh: -3,
+  },
+  {
+    title: "Ask a friend how to do it",
+    prog: 3,
+    fr: -1,
+  },
+  {
+    title: "Try to do the work",
+    prog: 1,
+    mh: -1,
+  },
+  {
+    title: "Apply for a promotion",
+    mh: -3,
+    mgr: 2,
+  },
+  {
+    title: "Learn a few buzzwords",
+    mgr: 1,
+    prog: 1,
+    time: 1,
+  },
+];
